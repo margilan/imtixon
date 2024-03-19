2f19821a8ae41298290ca88d7f01227aca90c142
margilan <shermatovelbek88@gmail.com>
2024-03-19 06:00:41 +0500

    imitixon

--- src/pages/Add/index.jsx
+++ src/pages/Add/index.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import kal from "../../assets/ADD.png";
 import "./index.css";
+import boy from "../../assets/akaxo.png";
+import insta from '../../assets/insta.svg'
+import feec from '../../assets/feecbook.svg'
+import twiter from '../../assets/twiter.svg'
+
 
 function ADD() {
   const [quantity, setQuantity] = useState(1);
@@ -48,6 +53,63 @@ function ADD() {
             </button>
             <button className="ADD-btn">ADD TO CART</button>
           </div>
+        </div>
+   
+      </div>
+      <div className="big-card">
+        <div className="big-card-texts">
+          <div className="one-card-text">
+            <h2>
+              Bringing you the <br /> <span>best</span> audio gear
+            </h2>
+          </div>
+          <div className="two-card-text">
+            <h3>
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </h3>
+          </div>
+        </div>
+        <div className="images">
+          <img src={boy} alt="" />
+        </div>
+      </div>
+
+      <div className="footer-big">
+        <div className="footer-big-texts">
+          <div className="footer-big-texts-one">
+            <h2>audiophile</h2>
+          </div>
+          <div className="footer-big-texts-two">
+            <h3>
+              Audiophile is an all in one stop to fulfill your audio needs.
+              We're a small team of music lovers and sound specialists who are
+              devoted to helping you get the most out of personal audio. Come
+              and visit our demo facility - we’re open 7 days a week.
+            </h3>
+          </div>
+          <div className="footer-big-texts-three">
+            <h3>Copyright 2021. All Rights Reserved</h3>
+          </div>
+        </div>
+        <div className="logos">
+          <div className="textts">
+            <h1 className="Aboutt">Home</h1>
+            <h1 className="Aboutt">HEADPHONES</h1>
+            <h1 className="Aboutt">SPEAKERS</h1>
+            <h1 className="Aboutt">EARPHONES</h1>
+          </div>
+
+              <div className="logo">
+                <img src={feec} alt="" />
+                <img src={twiter} alt="" />
+                <img src={insta} alt="" />
+              </div>
         </div>
       </div>
     </>
